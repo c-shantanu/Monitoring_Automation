c8dd2e030a06fc7a4d1fa5087a8074f678359cba
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-18 13:31:46 +0530

    Update main.tf

--- prometheus_infra/main.tf
+++ prometheus_infra/main.tf
@@ -1,100 +1,29 @@
-provider "aws" {
-    region = "ap-southeast-1"
+module "netwoking" {
+    source = "./network"
+    vpc_cidr = var.vpccidr
+    vpc_name = var.vpcname
+    tenancy = var.vpctenancy
+    public_subnet_names = var.pub_sub_names
+    pub_cidr = var.pubcidr
+    private_subnet_names = var.pvt_sub_names
+    pv_cidr = var.pvtcidr
+    igw_name = var.igwname
+    nat_name = var.natname
+    public_route_table_names = var.public_rt_names
+    private_route_table_names = var.private_rt_names  
 }
 
-#securitygroup using Terraform
-
-resource "aws_security_group" "TF_SG" {
-  name        = "metrics SG"
-  description = "metrics security group using Terraform"
-  vpc_id      = "vpc-0a5cc043287a0b892"
-
-  ingress {
-    description      = "prometheus"
-    from_port        = 9090
-    to_port          = 9090
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "grafana"
-    from_port        = 3000
-    to_port          = 3000
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "prometheus Node Exporter"
-    from_port        = 9100
-    to_port          = 9100
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "http"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "TF_SG"
-  }
+module "Security" {
+    source = "./security"
+    bastion_instance_name = var.pub_instance_name
+    private_instance_name = var.pvt_instance_name
+    instance_type = var.instancetype
+    seq_grp = var.seqgrp
+    key_name = var.keyname
+    subnet_pub_id = module.netwoking.pub_subnet_id[0]
+    subnet_pvt_id = module.netwoking.pri_subnet_id
+    vpc_id = module.netwoking.vpc_id
+  
 }
-
-#instances using Terraform
-
-resource "aws_instance" "web1" {
-  ami             = "ami-03caf91bb3d81b843"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.TF_SG.name]
-  key_name = data.aws_key_pair.existing_key_pair.key_name
-
-  tags = {
-    Name = "prometheus+grafana"
-  }
-}
-
-
-resource "aws_instance" "web2" {
-  ami             = "ami-03caf91bb3d81b843"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.TF_SG.name]
-  key_name = data.aws_key_pair.existing_key_pair.key_name
-
-  tags = {
-    Name = "node_exporter"
-  }
-}
-
-
-
-
-
 
 
